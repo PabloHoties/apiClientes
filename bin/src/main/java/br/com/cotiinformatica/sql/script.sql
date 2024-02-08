fec035d5f1427fae01ebb301c93dedc8644007c7
PabloHoties <pablohotiesmarques@gmail.com>
2024-02-08 12:51:35 -0300

    feat: Desenvolvimento dos endpoints de clientes e planos

---
+++ bin/src/main/java/br/com/cotiinformatica/sql/script.sql
@@ -0,0 +1,24 @@
+--Criar a tabela de planos
+create table plano(
+	id		uuid			primary key,
+	nome	varchar(25)		not null unique
+);
+
+--Criar a tabela de clientes
+create table cliente(
+	id			uuid			primary key,
+	nome		varchar(150)	not null,
+	telefone	varchar(15)		not null,
+	email		varchar(50)		not null,
+	plano_id	uuid			not null,
+	foreign key(plano_id)
+		references plano(id)
+);
+
+--Cadastrar planos
+insert into plano(id, nome) values(gen_random_uuid(), 'Plano pessoa física');
+insert into plano(id, nome) values(gen_random_uuid(), 'Plano pessoa jurídica');
+insert into plano(id, nome) values(gen_random_uuid(), 'Plano pessoa preferencial');
+insert into plano(id, nome) values(gen_random_uuid(), 'Plano pessoa vip');
+
+select * from plano;
--- src/main/java/br/com/cotiinformatica/repositories/ClienteRepository.java
+++ src/main/java/br/com/cotiinformatica/repositories/ClienteRepository.java
@@ -63,7 +63,10 @@ public class ClienteRepository {
 
 		Connection connection = ConnectionFactory.getConnection();
 
-		PreparedStatement statement = connection.prepareStatement("select * from cliente order by nome");
+		PreparedStatement statement = connection.prepareStatement
+				("select c.id, c.nome, c.email, c.telefone, p.id as idplano, p.nome as nomeplano "
+						+ "from cliente c inner join plano p on p.id = c.plano_id "
+						+ "order by c.nome");
 
 		ResultSet resultSet = statement.executeQuery();
 
@@ -77,7 +80,8 @@ public class ClienteRepository {
 			cliente.setNome(resultSet.getString("nome"));
 			cliente.setEmail(resultSet.getString("email"));
 			cliente.setTelefone(resultSet.getString("telefone"));
-			cliente.getPlano().setId(UUID.fromString(resultSet.getString("plano_id")));
+			cliente.getPlano().setId(UUID.fromString(resultSet.getString("idplano")));
+			cliente.getPlano().setNome(resultSet.getString("nomeplano"));
 
 			lista.add(cliente);
 
@@ -91,7 +95,10 @@ public class ClienteRepository {
 
 		Connection connection = ConnectionFactory.getConnection();
 
-		PreparedStatement statement = connection.prepareStatement("select * from cliente where id=?");
+		PreparedStatement statement = connection.prepareStatement
+				("select c.id, c.nome, c.email, c.telefone, p.id as idplano, p.nome as nomeplano "
+						+ "from cliente c inner join plano p on p.id = c.plano_id "
+						+ "where c.id = ?");
 		statement.setObject(1, id);
 		ResultSet resultSet = statement.executeQuery();
 
@@ -105,7 +112,8 @@ public class ClienteRepository {
 			cliente.setNome(resultSet.getString("nome"));
 			cliente.setEmail(resultSet.getString("email"));
 			cliente.setTelefone(resultSet.getString("telefone"));
-			cliente.getPlano().setId(UUID.fromString(resultSet.getString("plano_id")));
+			cliente.getPlano().setId(UUID.fromString(resultSet.getString("idplano")));
+			cliente.getPlano().setNome(resultSet.getString("nomeplano"));
 
 		}
 		
